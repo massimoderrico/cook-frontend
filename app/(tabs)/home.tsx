bf8e071cd794115449f97dfef112650308a08af6
letaoli123 <ericletao@yahoo.ca>
2025-02-01 23:29:52 -0500

    add files

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,14 @@
+import { ThemedText } from "@/components/ThemedText";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useThemeColor } from "@/hooks/useThemeColor";
+
+export default function Home(){
+    const backgroundColor = useThemeColor("background")
+
+    return (
+        <SafeAreaView
+        style={{backgroundColor:backgroundColor}}>
+            <ThemedText>Hello</ThemedText>
+        </SafeAreaView>
+    )
+}
