d1b477225a9398c523808a2c6cd084662b85246f
letaoli123 <ericletao@yahoo.ca>
2025-03-09 22:12:38 -0400

    frontend dev

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,6 +1,7 @@
 import { ThemedText } from "@/components/ThemedText";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useThemeColor } from "@/hooks/useThemeColor";
+import HomePage from "@/components/navigation/HomePage";
 
 export default function Home(){
     const backgroundColor = useThemeColor("background")
@@ -8,7 +9,7 @@ export default function Home(){
     return (
         <SafeAreaView
         style={{backgroundColor:backgroundColor, flex: 1}}>
-            <ThemedText>Home</ThemedText>
+            <HomePage/>
         </SafeAreaView>
     )
 }
